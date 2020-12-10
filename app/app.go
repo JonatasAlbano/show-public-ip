5393526cd1850026bbd9d907ccc981591ecc6cd7
Jonatas Albano <jonatas_albano@hotmail.com>
2020-12-09 22:05:50 -0300

    Adding flag servers

--- app/app.go
+++ app/app.go
@@ -14,23 +14,30 @@ func Generate() *cli.App {
 	app.Name = "Command line application"
 	app.Usage = "Search IPs and Server names in web"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com.br",
+		},
+	}
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search IPs and adresses in web",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com.br",
-				},
-			},
-			Action: searchIp,
+			Name:   "ip",
+			Usage:  "Search IPs and adresses in web",
+			Flags:  flags,
+			Action: searchIP,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Search the server name",
+			Flags:  flags,
+			Action: searchServers,
 		},
 	}
 	return app
 }
 
-func searchIp(c *cli.Context) {
+func searchIP(c *cli.Context) {
 	host := c.String("host")
 
 	ips, erro := net.LookupIP(host)
@@ -40,5 +47,18 @@ func searchIp(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
